5c45e68519d0a9f2e206c0e6a16416a926ce2467
frobbins <robbinsfatim@gmail.com>
2020-08-23 21:24:06 -0600

    Updated the contact info code3

--- Develop/README.md
+++ Develop/README.md
@@ -1,30 +1,40 @@
-# Readme generator
-# n/a
-##  Description 
-produce a good readme
-# ReadMe Generator
-# n/a
-##  Description 
-Allows the user to quickly produce a quality ReadMe 
-# ReadMe Generator
-# n/a
-##  Description 
-Create a quality ReadMe
-##  Table of Contents 
--  Title,Description,Instructions, Usage License, How to contribute, Authors, How to run tess, Contact Info
-##  Installation Instructions 
-Create a quality ReadMe
-##  Usage 
-Create a quality ReadMe
-##  License 
-Create a quality ReadMe
-##  How to Contribute 
-Create a quality ReadMe
-##  Authors 
-Create a quality ReadMe
-##  How to Run Tests
-Create a quality ReadMe
-##  Contact Info
-![alt text](Create a quality ReadMe
+##Title 
+Readme
+##Description 
+n/a
+##Table of Contents 
+-  n/a
+##Installation Instructions 
+n/a
+##Usage 
+n/a
+##License 
 
+##How to Contribute 
 undefined
+##Authors 
+n/a
+##How to Run Tests
+n/a
+##Contact Info
+![alt text](undefined
+##Title 
+ReadME Generator
+##Description 
+n/a
+##Table of Contents 
+-  n
+##Installation Instructions 
+n
+##Usage 
+n
+##License 
+
+##How to Contribute 
+undefined
+##Authors 
+n
+##How to Run Tests
+n
+##Contact Info
+![alt text](undefined
--- Develop/index.js
+++ Develop/index.js
@@ -6,12 +6,6 @@ const util = require("util");
 
 // array of questions for user
 const questions = [
-
-        {
-            type: "input",
-            name: "badge",
-            message: "What is your badge code?"
-        },
         {
             type: "input",
             name: "title",
@@ -34,7 +28,7 @@ const questions = [
         },
         {
             type: "checkbox",
-            name: "License",
+            name: "license",
             message: "Choose your License",
             choices: [
                 'Apache', 'MIT', 'GPL', 'BSD'
@@ -47,7 +41,7 @@ const questions = [
         },
         {
             type: "input",
-            name: "authors",
+            name: "author",
             message: "Please list the author/authors"
         },
         {
@@ -57,13 +51,8 @@ const questions = [
         },
         {
             type: "input",
-            name: "questions1",
-            message: "Enter photo url"
-        },
-        {
-            type: "input",
-            name: "question2",
-            message: "Enter email"
+            name: "contact",
+            message: "Enter your contact infol"
         },
         {
             type: "input",
@@ -76,38 +65,32 @@ console.clear();
 
 inquirer
     .prompt(questions).then( response => {
-        fs.appendFileSync("README.md", ("# " + response.title) + '\n', function (err) {
+        fs.appendFileSync("README.md", ("##Title " +  '\n' + response.title) + '\n', function (err) {
             if (err) {
                 return console.log(err);
             }
             console.log("It works");
         });
-        fs.appendFileSync("README.md", ("# " + response.badge) + '\n', function (err) {
+        fs.appendFileSync("README.md", ("##Description " + '\n' + response.description) + '\n', function (err) {
             if (err) {
                 return console.log(err);
             }
             console.log("It works");
         });
-        fs.appendFileSync("README.md", ("##  Description " + '\n' + response.description) + '\n', function (err) {
+        fs.appendFileSync("README.md", ("##Table of Contents " + '\n' + '-  ' + response.toc.split(",  ").join('\n' + '-  ')) + '\n', function (err) {
             if (err) {
                 return console.log(err);
             }
             console.log("It works");
         });
-        fs.appendFileSync("README.md", ("##  Table of Contents " + '\n' + '-  ' + response.toc.split(",  ").join('\n' + '-  ')) + '\n', function (err) {
-            if (err) {
-                return console.log(err);
-            }
-            console.log("It works");
-        });
-        fs.appendFileSync("README.md", ("##  Installation Instructions " + '\n' + response.description) + '\n', function (err) {
+        fs.appendFileSync("README.md", ("##Installation Instructions " + '\n' + response.installation) + '\n', function (err) {
             if (err) {
                 return console.log(err);
             }
             console.log("It works");
             });
           
-        fs.appendFileSync("README.md", ("##  Usage " + '\n' + response.description) + '\n', function (err) {
+        fs.appendFileSync("README.md", ("##Usage " + '\n' + response.usage) + '\n', function (err) {
             if (err) {
                 return console.log(err);
             }
@@ -115,38 +98,32 @@ inquirer
                 });
              
 
-         fs.appendFileSync("README.md", ("##  License " + '\n' + response.description) + '\n', function (err) {
+         fs.appendFileSync("README.md", ("##License " + '\n' + response.license) + '\n', function (err) {
                     if (err) {
                         return console.log(err);
                     }
                     console.log("It works");
                 });
 
-        fs.appendFileSync("README.md", ("##  How to Contribute " + '\n' + response.description) + '\n', function (err) {
+        fs.appendFileSync("README.md", ("##How to Contribute " + '\n' + response.contribute) + '\n', function (err) {
               if (err) {
                 return console.log(err);
                  }
          console.log("It works");
     });
-    fs.appendFileSync("README.md", ("##  Authors " + '\n' + response.description) + '\n', function (err) {
+    fs.appendFileSync("README.md", ("##Authors " + '\n' + response.author) + '\n', function (err) {
         if (err) {
           return console.log(err);
            }
    console.log("It works");
 });
-fs.appendFileSync("README.md", ("##  How to Run Tests" + '\n' + response.description) + '\n', function (err) {
+fs.appendFileSync("README.md", ("##How to Run Tests" + '\n' + response.test) + '\n', function (err) {
     if (err) {
       return console.log(err);
        }
 console.log("It works");
 });
-fs.appendFileSync("README.md", ("##  Contact Info" + '\n' + "![alt text](" + response.description) + '\n', function (err) {
-    if (err) {
-      return console.log(err);
-       }
-console.log("It works");
-});
-fs.appendFileSync("README.md", ('\n' + response.questions2) + '\n', function (err) {
+fs.appendFileSync("README.md", ("##Contact Info" + '\n' + response.contact) + '\n', function (err) {
     if (err) {
       return console.log(err);
        }
